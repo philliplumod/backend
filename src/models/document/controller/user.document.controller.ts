1cf8fcd71639e67ab0b390a4790c5f64f24f2d99
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-09-14 10:37:09 +0800

    merge conflict fixed

--- src/models/document/controller/user.document.controller.ts
+++ src/models/document/controller/user.document.controller.ts
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
-import { Controller, Post, Get, Body, Param, HttpException, HttpStatus, Logger, ParseUUIDPipe } from '@nestjs/common';
-import { UserDocument } from '../entities/document.entity';
-import { CreateDocumentDto } from '../user.document.dto';
-import { DocumentService } from '../user.document.service';
-=======
 import { Controller, Post, Body, Get, Param, HttpException, HttpStatus } from '@nestjs/common';
 import { DocumentService } from '../user.document.service';
 import { CreateDocumentDto } from '../user.document.dto';
 import { UserDocument } from '../entities/document.entity';
 
->>>>>>> origin/main
 
 
 @Controller('document')
 export class DocumentController {
-<<<<<<< HEAD
-  private readonly logger = new Logger(DocumentController.name);
-
-  constructor(private readonly documentService: DocumentService) {}
-
-  @Post('upload')
-  async uploadDocument(@Body() createDocumentDto: CreateDocumentDto): Promise<UserDocument> {
-=======
   constructor(private readonly documentService: DocumentService) {}
 
   @Post('upload')
   async uploadDocument(@Body() createDocumentDto: CreateDocumentDto): Promise<UserDocument> {  // Updated type
->>>>>>> origin/main
     try {
       return await this.documentService.uploadDocument(createDocumentDto);
     } catch (error) {
@@ -35,17 +19,6 @@ export class DocumentController {
   }
 
   @Get('user/:userId')
-<<<<<<< HEAD
-  async getDocumentsByUser(@Param('userId', new ParseUUIDPipe()) userId: string): Promise<UserDocument[]> {
-    try {
-      return await this.documentService.getDocumentsByUser(userId);
-    } catch (error) {
-      this.logger.error(`Error fetching documents for user ${userId}: ${error.message}`, error.stack);
-      throw new HttpException('Error fetching documents', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
-}
-=======
   async getDocumentsByUser(@Param('userId') userId: string): Promise<UserDocument[]> {  // Updated type
     try {
       return await this.documentService.getDocumentsByUser(userId);
@@ -54,4 +27,3 @@ export class DocumentController {
     }
   }
 }
->>>>>>> origin/main
