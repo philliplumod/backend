75ca619d220f553a6a581eaa221ec610e7456eaa
Ricric <jericksignapan3331@gmail.com>
2024-10-14 17:47:18 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpErrorFilter } from './handler/http-error.filter';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const port = process.env.PORT || 3000; 
 async function bootstrap() {
-  dotenv.config();
-  
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.useGlobalFilters(new HttpErrorFilter());
-  await app.listen(3000);
-} 
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Motorcycle Rental API')
+    .setDescription('The Mobi HRIS API description')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+}
 bootstrap();
